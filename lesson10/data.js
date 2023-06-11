526f104b2882e43ccc0801d5d222d79b90644926
Maks <kpasowsky@gmail.com>
2023-06-11 14:44:14 +0300

    Д.з. по 10 семинару

---
+++ lesson10/data.js
@@ -0,0 +1,19 @@
+const data = `
+    [
+        {
+            "answer": "yes",
+            "forced": false,
+            "image": "https://yesno.wtf/assets/yes/8-2f93962e2ab24427df8589131da01a4d.gif"
+        },
+        {
+            "answer": "no",
+            "forced": false,
+            "image": "https://yesno.wtf/assets/no/17-829284e9dd894ce9fb65fbe86d2e382c.gif"
+        },
+        {
+            "answer": "yes",
+            "forced": false,
+            "image": "https://yesno.wtf/assets/yes/14-b57c6dc03aa15a4b18f53eb50d6197ee.gif"
+        }
+    ]
+`
