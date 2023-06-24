ed5b80bef2119e3d249adceda58ba5484532cc01
Maks <kpasowsky@gmail.com>
2023-06-25 00:12:51 +0300

    Итоговое задание по курсу JS

---
+++ lesson13/datacards.js
@@ -0,0 +1,88 @@
+const dataProductsInfo = `
+[
+    {
+        "image": "img/img1.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "basket": {
+            "image": "img/foto1.jpg",
+            "title": "MANGO  PEOPLE  T-SHIRT",
+            "price": "100",
+            "color": "Red",
+            "size": "Xl",
+            "quantity": "2"
+        }
+    },
+    {
+        "image": "img/img2.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "basket": {
+            "image": "img/foto2.jpg",
+            "title": "MANGO  PEOPLE  T-SHIRT",
+            "price": "200",
+            "color": "Red",
+            "size": "Xl",
+            "quantity": "2"
+        }
+    },
+    {
+        "image": "img/img3.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "basket": {
+            "image": "img/foto1.jpg",
+            "title": "MANGO  PEOPLE  T-SHIRT",
+            "price": "300",
+            "color": "Red",
+            "size": "Xl",
+            "quantity": "2"
+        }
+    },
+    {
+        "image": "img/img4.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "basket": {
+            "image": "img/foto2.jpg",
+            "title": "MANGO  PEOPLE  T-SHIRT",
+            "price": "400",
+            "color": "Red",
+            "size": "Xl",
+            "quantity": "2"
+        }
+    },
+    {
+        "image": "img/img5.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "basket": {
+            "image": "img/foto1.jpg",
+            "title": "MANGO  PEOPLE  T-SHIRT",
+            "price": "500",
+            "color": "Red",
+            "size": "Xl",
+            "quantity": "2"
+        }
+    },
+    {
+        "image": "img/img6.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "basket": {
+            "image": "img/foto2.jpg",
+            "title": "MANGO  PEOPLE  T-SHIRT",
+            "price": "600",
+            "color": "Red",
+            "size": "Xl",
+            "quantity": "2"
+        }
+    }
+]
+`
